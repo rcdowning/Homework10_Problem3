e19351b5f19c2f964a045741ee1a132564c4b1e1
rcdowning <111823343+rcdowning@users.noreply.github.com>
2022-11-02 05:35:08 +0000

    Mapping out problem with comments

---
+++ .ccls-cache/@home@runner@Problem-3-1/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int N;
+
+  cin >> N;
+
+  // declare arrays
+
+  // for loop to cycle through array
+
+  // loop to determine larger value in array, way to keep count?
+
+  // output the number of indexes and the array of values left
+
+  
+
+
+
+
+  
+}
