7cb09d6bbcee174d4104a5d5b8abc72dc2564d7f
rcdowning <111823343+rcdowning@users.noreply.github.com>
2022-11-02 05:40:41 +0000

    looping to compare values in array

--- main.cpp
+++ main.cpp
@@ -8,10 +8,26 @@ int main() {
   cin >> N;
 
   // declare arrays
+  int A[N];
 
   // for loop to cycle through array
+  for (int i = 0; i < N; i ++){
+    cin >> A[i];
+  }
 
   // loop to determine larger value in array, way to keep count?
+  int larger = 0;
+  int count = 0;
+
+  for (int k = 0; k < N; k++){
+    if(A[k] > larger){
+      
+    }
+  }
+
+  
+
+  
 
   // output the number of indexes and the array of values left
 
